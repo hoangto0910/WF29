e9ad815052a268827a943d39eff32d11a8ad4f74
hoangto0910 <dangtohoang@gmail.com>
2020-01-07 21:01:05 +0700

    sua bai 9.1

--- BTVNbuoi9/js/9.1.js
+++ BTVNbuoi9/js/9.1.js
@@ -1,24 +1,24 @@
-function Showtab () {
-	var tab1 = document.getElementById('tab1');
-	tab1.style.display = 'block';
-	var tab2 = document.getElementById('tab2');
-	tab2.style.display = 'none';
-	var tab3 = document.getElementById('tab3');
-	tab3.style.display = 'none';
-}
-function Showtab2() {
-	var tab2 = document.getElementById('tab2');
-	tab2.style.display = 'block';
-	var tab1 = document.getElementById('tab1');
-	tab1.style.display = 'none';
-	var tab3 = document.getElementById('tab3');
-	tab3.style.display = 'none';
-}
-function Showtab3() {
-	var tab3 = document.getElementById('tab3');
-	tab3.style.display = 'block';
-	var tab2 = document.getElementById('tab2');
-	tab2.style.display = 'none';
-	var tab1 = document.getElementById('tab1');
-	tab1.style.display = 'none';
-}
+// function Showtab () {
+// 	var tab1 = document.getElementById('tab1');
+// 	tab1.style.display = 'block';
+// 	var tab2 = document.getElementById('tab2');
+// 	tab2.style.display = 'none';
+// 	var tab3 = document.getElementById('tab3');
+// 	tab3.style.display = 'none';
+// }
+// function Showtab2() {
+// 	var tab2 = document.getElementById('tab2');
+// 	tab2.style.display = 'block';
+// 	var tab1 = document.getElementById('tab1');
+// 	tab1.style.display = 'none';
+// 	var tab3 = document.getElementById('tab3');
+// 	tab3.style.display = 'none';
+// }
+// function Showtab3() {
+// 	var tab3 = document.getElementById('tab3');
+// 	tab3.style.display = 'block';
+// 	var tab2 = document.getElementById('tab2');
+// 	tab2.style.display = 'none';
+// 	var tab1 = document.getElementById('tab1');
+// 	tab1.style.display = 'none';
+// }
